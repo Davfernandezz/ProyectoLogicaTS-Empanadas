68267f6972a0845d78ca2b3e01e84d551eab527f
David Fernadez <daffevafernandez@gmail.com>
2024-06-13 20:07:07 +0200

    conditions for input parameters

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-export const fn = (/* argumentos de la función */) => {
+export const fn = (a, b, c) => {
    // Cuerpo de la función: Completa la lógica de la función aquí.
+   
+   if(a + b + c > 40) {
+      throw Error("El número de empanadas no puede pasar de 40.")
+   }
+
+   if((a + b + c) % 3 !== 0) {
+      throw Error("El número de empanadas tiene que ser divisible entre 3.")
+   }
+
+   if(a < 0 || b < 0 || c < 0) {
+      throw Error("El numero de empanadas tiene que ser un numero entero.")
+   }
+
 };
