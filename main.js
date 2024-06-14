eeb4d7ad568da12864c337a1740f9bc6bc395c7a
David Fernadez <daffevafernandez@gmail.com>
2024-06-14 20:46:48 +0200

    I add .push to allow us to add a new element to the end of the array

--- main.js
+++ main.js
@@ -2,26 +2,26 @@ export const fn = (a, b, c) => {
    // Cuerpo de la función: Completa la lógica de la función aquí.
    
    if(a + b + c > 40) {
-      throw Error("El número de empanadas no puede pasar de 40.")
+      throw new Error("El número de empanadas no puede pasar de 40.")
    }
 
    if((a + b + c) % 3 !== 0) {
-      throw Error("El número de empanadas tiene que ser divisible entre 3.")
+      throw new Error("El número de empanadas tiene que ser divisible entre 3.")
    }
 
    if(a < 0 || b < 0 || c < 0) {
-      throw Error("El numero de empanadas tiene que ser un numero entero.")
+      throw new Error("El numero de empanadas tiene que ser un numero entero.")
    }
 
    let preciosEmpanadas = [];
    for (let i = 0; i < a; i++) {
-      preciosEmpanadas(12);
+      preciosEmpanadas.push(12);
    }
    for (let i = 0; i < b; i++) {
-      preciosEmpanadas(14);
+      preciosEmpanadas.push(14);
    }
    for (let i = 0; i < c; i++) {
-      preciosEmpanadas(16);
+      preciosEmpanadas.push(16);
    }
 
    console.log(preciosEmpanadas);
